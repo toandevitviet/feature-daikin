747353314d6c9e769ee50b84e9ace97c97498ee1
ToanLM <toandevitviet@gmail.com>
2015-03-30 21:15:11 +0700

    Update logo and left right

--- js/process.js
+++ js/process.js
@@ -1,176 +1,33 @@
-/*page1*/
-timer1 = setTimeout(function () {
-    $('#div-center').removeClass('center-page');
-}, 500);
+function listener(e) {
+    var l = document.createElement("li");
+    switch(e.type) {
+        case "animationstart":
+            l.innerHTML = "Started: elapsed time is " + e.elapsedTime;
+            break;
+        case "animationend":
+            l.innerHTML = "Ended: elapsed time is " + e.elapsedTime;
+            break;
+        case "animationiteration":
+            l.innerHTML = "New loop started at time " + e.elapsedTime;
+            break;
+    }
+    document.getElementById("output").appendChild(l);
+}
 
-timer2 = setTimeout(function () {
-    $('#div-content').removeClass('content-page');
-}, 1000);
+function left() {
+    var e = document.getElementById("div-content-1");
+    e.addEventListener("animationstart", listener, false);
+    e.addEventListener("animationend", listener, false);
+    e.addEventListener("animationiteration", listener, false);
 
-timer3 = setTimeout(function () {
-    $('#div-content-1').removeClass('left-content');
-}, 2000);
+    e.className = "slidein-left";
+}
 
-timer4 = setTimeout(function () {
-    $('#div-content-2').removeClass('right-content');
-}, 2000);
-/*page1*/
+function right() {
+    var e = document.getElementById("div-content-2");
+    e.addEventListener("animationstart", listener, false);
+    e.addEventListener("animationend", listener, false);
+    e.addEventListener("animationiteration", listener, false);
 
-/*page2*/
-timer12 = setTimeout(function () {
-    $('#page2-logo').removeClass('page2-logo');
-}, 500);
-
-timer22 = setTimeout(function () {
-    $('#page2-text').removeClass('page2-textc');
-    alert("1");
-}, 1500);
-
-timer32 = setTimeout(function () {
-    $('#page2-left').removeClass('page2-leftc'); alert("2");
-}, 2500);
-
-timer42 = setTimeout(function () {
-    $('#page2-right').removeClass('page2-rightc'); alert("3");
-}, 2500);
-/*page2*/
-
-/*page3*/
-timer13 = setTimeout(function () {
-    $('#page3-logo').removeClass('page3-logo');
-}, 1000);
-
-timer23 = setTimeout(function () {
-    $('#page3-text').removeClass('page3-text');
-}, 3000);
-
-timer33 = setTimeout(function () {
-    $('#page3-content').removeClass('page3-content');
-}, 5000);
-
-timer43 = setTimeout(function () {
-    $('#li-1').removeClass('li-1');
-}, 6000);
-
-timer53 = setTimeout(function () {
-    $('#li-2').removeClass('li-2');
-}, 7000);
-
-timer63 = setTimeout(function () {
-    $('#li-3').addClass('li-3-is');
-}, 8000);
-
-timer73 = setTimeout(function () {
-    $('#li-4').removeClass('li-4');
-}, 9000);
-
-timer83 = setTimeout(function () {
-    $('#li-5').addClass('li-5-is');
-}, 10000);
-
-timer93 = setTimeout(function () {
-    $('#li-6').removeClass('li-6');
-}, 11000);
-
-timer103 = setTimeout(function () {
-    $('#li-7').removeClass('li-7');
-}, 12000);
-/*page3*/
-
-/*page4*/
-timer14 = setTimeout(function () {
-    $('#page4-logo').removeClass('page4-logo');
-}, 1000);
-
-timer24 = setTimeout(function () {
-    $('#page4-text').removeClass('page4-text');
-}, 2000);
-
-timer34 = setTimeout(function () {
-    $('#page4-content').removeClass('page4-content');
-}, 3000);
-/*page4*/
-
-/*page5*/
-timer15 = setTimeout(function () {
-    $('#page5-logo').removeClass('page5-logo');
-}, 1000);
-
-timer25 = setTimeout(function () {
-    $('#page5-text').removeClass('page5-text');
-}, 3000);
-
-timer35 = setTimeout(function () {
-    $('#page5-content').removeClass('page5-content');
-}, 3000);
-
-timer45 = setTimeout(function () {
-    $('#image1').removeClass('image1');
-}, 4000);
-
-timer55 = setTimeout(function () {
-    $('#image2').removeClass('image2');
-}, 5000);
-/*page5*/
-
-/*page6*/
-timer16 = setTimeout(function () {
-    $('#page6-logo').removeClass('page6-logo');
-}, 1000);
-
-timer26 = setTimeout(function () {
-    $('#page6-text').removeClass('page6-text');
-}, 3000);
-
-timer36 = setTimeout(function () {
-    $('#page6-content').removeClass('page6-content');
-}, 3000);
-
-timer46 = setTimeout(function () {
-    $('#image1').removeClass('image1');
-}, 4000);
-
-timer56 = setTimeout(function () {
-    $('#image2').removeClass('image2');
-}, 5000);
-/*page6*/
-
-/*page7*/
-timer17 = setTimeout(function () {
-    $('#page7-logo').removeClass('page7-logo');
-}, 1000);
-
-timer27 = setTimeout(function () {
-    $('#page7-text').removeClass('page7-text');
-}, 3000);
-
-timer37 = setTimeout(function () {
-    $('#page7-content').removeClass('page7-content');
-}, 3000);
-
-timer47 = setTimeout(function () {
-    $('#image1').removeClass('image1');
-}, 4000);   
-/*page7*/
-
-/*page8*/
-timer18 = setTimeout(function () {
-    $('#page8-logo').removeClass('page8-logo');
-}, 1000);
-
-timer28 = setTimeout(function () {
-    $('#page8-text').removeClass('page8-text');
-}, 3000);
-
-timer38 = setTimeout(function () {
-    $('#page8-content').removeClass('page8-content');
-}, 3000);
-
-timer48= setTimeout(function () {
-    $('#image1').removeClass('image1');
-}, 4000);
-
-timer58 = setTimeout(function () {
-    $('#image2').removeClass('image2');
-}, 5000);
-/*page8*/
+    e.className = "slidein-right";
+}
