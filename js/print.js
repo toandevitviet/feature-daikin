904562936fd468f13eed04285d0854ce25890aa3
ToanLM <toandevitviet@gmail.com>
2015-04-04 16:04:11 +0700

    Update code print page

--- js/print.js
+++ js/print.js
@@ -4,4 +4,6 @@ function printContent(divName) {
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
+    //settimeout refresh, conflix code print reload page ( next, prev )
+    var timeout = setTimeout("location.reload(true);",100);
 }
